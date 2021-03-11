1c07f065ebf619f04ef9c484ccb2bdbb122d8550
Joshua Parado <jpa3ado@gmail.com>
2021-03-11 00:01:26 -0500

    done

--- index.js
+++ index.js
@@ -28,9 +28,10 @@ console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
   Study the code for counter1 and counter2, then answer the questions below.
   
   1. What is the difference between counter1 and counter2?
+  counter1 starts out with a function, counter2 uses a closure. 
   
   2. Which of the two uses a closure? How can you tell?
-  
+  counter 2 used a closer. I can tell because a variable is defined first. 
   3. In what scenario would the counter1 code be preferable? In what scenario would 
      counter2 be better?  
 */
@@ -62,9 +63,10 @@ Use the inning function below to do the following:
 NOTE: This will be a callback function for the tasks below
 */
 
-function inning(/*Code Here*/){
-    /*Code Here*/
+function inning(){
+  return Math.floor(Math.random() * 3)
 }
+console.log(inning())
 
 
 /* ⚾️⚾️⚾️ Task 3: finalScore() ⚾️⚾️⚾️
@@ -81,19 +83,37 @@ Use the finalScore function below to do the following:
 }
 */ 
 
-function finalScore(/*code Here*/){
-  /*Code Here*/
+function finalScore(callBack, inning){
+  let home = 0;
+  let away = 0;
+
+  for (let i = 0; i < inning; i++){
+    home = home + callBack()
+    away = away + callBack()
+  }
+
+  return {
+    Home: home,
+    Away: away
+  }
+ 
 }
+finalScore(inning,9);
+
+
 
 /* ⚾️⚾️⚾️ Task 4: getInningScore() ⚾️⚾️⚾️
 Use the getInningScore() function below to do the following:
   1. Receive a callback function - you will pass in the inning function from task 2 as your argument 
   2. Return an object with a score for home and a score for away that populates from invoking the inning callback function */
 
-function getInningScore(/*Your Code Here */) {
-  /*Your Code Here */
+function getInningScore(callBack) {
+  return {
+    Home: callBack(),
+    Away: callBack()
+  }
 }
-
+console.log(getInningScore(inning))
 
 /* ⚾️⚾️⚾️ Task 5: scoreboard() ⚾️⚾️⚾️
 Use the scoreboard function below to do the following:
